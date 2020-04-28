73e2fb95ea90a15dd8514714c821c039cc129266
davidcrc <david_drcr@hotmail.com>
2020-04-28 11:12:14 -0500

    ADD: Food items

--- src/index.js
+++ src/index.js
@@ -20,6 +20,7 @@ export default class App extends Component {
     this.state = {
       dataBanner:[],
       dataCategories:[],
+      dataFood:[],
       selectCatg:0
     }
   }
@@ -33,7 +34,8 @@ export default class App extends Component {
       this.setState({
         isLoading: false,
         dataBanner: responseJson.banner,
-        dataCategories: responseJson.categories
+        dataCategories: responseJson.categories,
+        dataFood:responseJson.food
       });
 
     })
@@ -53,6 +55,27 @@ export default class App extends Component {
       </TouchableOpacity>
     )
   }
+
+  _renderItemFood(item){
+    let catg = this.state.selectCatg
+    if(catg==0||catg==item.categorie)
+    {
+      return(
+        <TouchableOpacity style={styles.divFood}>
+          <Image
+            style={styles.imageFood}
+            resizeMode="contain"
+            source={{uri:item.image}} />
+            <View style={{height:((width/2)-20)-90, backgroundColor:'transparent', width:((width/2)-20)-10}} />
+            <Text style={{fontWeight:'bold',fontSize:22,textAlign:'center'}}>
+              {item.name}
+            </Text>
+            <Text>Descp Food and Details</Text>
+            <Text style={{fontSize:20,color:"green"}}>${item.price}</Text>
+          </TouchableOpacity>
+        )
+    }
+  }
   render() {
     return (
       <ScrollView>
@@ -71,12 +94,19 @@ export default class App extends Component {
               <View style={{height:20}} />
           </View>
 
-          <View style={{width:width, borderRadius:20, paddingVertical:20, backgroundColor:'white', alignItems:'center'}}>
-            <Text style={styles.titleCatg}>Cat select {this.state.selectCatg}</Text>
+          <View style={{width:width, borderRadius:20, paddingVertical:20, backgroundColor:'white'}}>
+            <Text style={styles.titleCatg}>Categories</Text>
             <FlatList
               horizontal={true}
               data={this.state.dataCategories}
               renderItem={({ item }) => this._renderItem(item)}
+              keyExtractor = { (item,index) => index.toString() }
+            />
+            <FlatList
+              //horizontal={true}
+              data={this.state.dataFood}
+              numColumns={2}
+              renderItem={({ item }) => this._renderItemFood(item)}
               keyExtractor = { (item,index) => index.toString() }
             />
             <View style={{height:20}} />
@@ -108,5 +138,25 @@ const styles = StyleSheet.create({
     fontWeight:'bold',
     textAlign:'center',
     marginBottom:10
-  } 
+  },
+  imageFood:{
+    width:((width/2)-20)-10,
+    height:((width/2)-20)-30,
+    backgroundColor:'transparent',
+    position:'absolute',
+    top:-45
+  },
+  divFood:{
+    width:(width/2)-20,
+    padding:10,
+    borderRadius:10,
+    marginTop:55,
+    marginBottom:5,
+    marginLeft:10,
+    alignItems:'center',
+    elevation:8,
+    shadowOpacity:0.3,
+    shadowRadius:50,
+    backgroundColor:'white',
+  }
 });
